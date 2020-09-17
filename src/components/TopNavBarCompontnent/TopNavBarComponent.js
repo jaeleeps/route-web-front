cf8d955bb73c37f248146611dad3cdff55e086f4
이재영 <jaeleeps@jaranda.kr>
2020-09-17 21:10:42 +0900

    TopNavBar publishing edit

--- src/components/TopNavBarCompontnent/TopNavBarComponent.js
+++ src/components/TopNavBarCompontnent/TopNavBarComponent.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import "./TopNavBarComponent.scss";
 
 const TopNavBarComponent = () => {
-
   return (
-
     <div className="top-nav-bar-wrapper">
       <div className="title-wrapper">
-        <Link to={`/`} activeClassName="current">ROUTE</Link>
+        <Link to={`/`} activeClassName="current">
+          ROUTE
+        </Link>
       </div>
+
       <div className="nav-tabs-wrapper">
         <div className="nav-item">
-          <Link to={`/about`} activeClassName="current">ABOUT US</Link>
+          <Link to={`/about`} activeClassName="current">
+            ABOUT US
+          </Link>
         </div>
-        <div className="nav-item">
+        {/* <div className="nav-item">
           <Link to={`/home`} activeClassName="current">HOME</Link>
+        </div> */}
+        <div className="nav-item">
+          <Link to={`/`} activeClassName="current">
+            MEMBERS
+          </Link>
         </div>
         <div className="nav-item">
-          <Link to={`/contact`} activeClassName="current">CONTACT</Link>
+          <Link to={`/`} activeClassName="current">
+            CAREER
+          </Link>
         </div>
+        <div className="nav-item">
+          <Link to={`/`} activeClassName="current">
+            SERVICE
+          </Link>
+        </div>
+        <div className="nav-item">
+          <Link to={`/contact`} activeClassName="current">
+            CONTACT
+          </Link>
+        </div>
+      </div>
+
+      <div className="short-cut-items-wrapper">
+        <div className="short-cut-item-icon-wrapper"></div>
+        <div className="short-cut-item-icon-wrapper"></div>
+        <div className="short-cut-item-icon-wrapper"></div>
       </div>
     </div>
   );
