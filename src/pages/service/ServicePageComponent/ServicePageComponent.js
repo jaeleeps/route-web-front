88cda2526403738f14db8c051db43ac18eebca65
이재영 <jaeleeps@jaranda.kr>
2020-09-22 01:18:33 +0900

    ServicePage data stringtable

--- src/pages/service/ServicePageComponent/ServicePageComponent.js
+++ src/pages/service/ServicePageComponent/ServicePageComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { TopNavBarComponent } from "../../../components/TopNavBarCompontnent/TopNavBarComponent";
 import { BottomFooterComponent } from "../../../components/BottomFooterComponent/BottomFooterComponent";
 import { StringTable } from "../../../service/StringTable";
@@ -6,6 +6,52 @@ import "./ServicePageComponent.scss";
 
 class ServicePageComponent extends Component {
   render() {
+
+    const serviceContentItems = StringTable.SERVICE.MAIN.CARDS.map((cardData) => {
+      const cardContent = cardData.CONTENTS.map((cardContent) => {
+        switch (cardContent.type) {
+          case "paragraph":
+            return (
+              <div className="paragraph">
+                {cardContent.items.map((innerContent) => (<div>{innerContent}</div>))}
+              </div>
+            )
+            break;
+          case "subtitle":
+            return (
+              <div className="subtitle">
+                {cardContent.items.map((innerContent) => (<div>{innerContent}</div>))}
+              </div>
+              )
+            break;
+          case "block":
+            const blockItems = cardContent.items.map((item) => {
+              const descriprions = item.descriptions.map((desc) => (<div className="block-description">{desc}</div>));
+              return (
+                <div className="block-item">
+                  <div className="block-title">{item.title}</div>
+                  {descriprions}
+                </div>
+              )
+            });
+            return (
+              <div className="block-wrapper">{blockItems}</div>
+            );
+            break;
+          default:
+            return null;
+        }
+      });
+      return (
+        <div className="service-card-wrapper shadow-3">
+          <div className="card-title-wrapper shadow-1">
+            <div className="title-text">{cardData.TITLE}</div> <div className="title-line"></div>
+          </div>
+          <div className="card-content-wrapper">{ cardContent }</div>
+        </div>
+      );
+    });
+
     return (
       <div className="service-page-component-wrapper page-component-wrapper">
         <div className="service-page-main-content-wrapper">
@@ -14,30 +60,21 @@ class ServicePageComponent extends Component {
           <div className="service-page-img-banner-wrapper"></div>
 
           <div className="service-page-main-content-inner-wrapper">
-            
             <div className="service-content-wrapper responsive-handler-wrapper">
-
               <div className="title-wrapper">
-                <div className="title-text">SERVICE</div>
+                <div className="title-text">{StringTable.SERVICE.TITLE}</div>
                 <div className="title-line"></div>
               </div>
 
-              <div className="logo-wrapper"></div>
+              <div className="desc-color-wrapper">
+                <div className="text-wrapper">{StringTable.SERVICE.DESCRIPTION}</div>
+              </div>
+
 
               <div className="service-text-wrapper">
-              <div className="paragraph-wrapper">
-                <span className="highlight">{StringTable.ABOUT.MAIN.PARAGRAPH_1.HIGHLIGHT}</span>
-                <span className="normal">{StringTable.ABOUT.MAIN.PARAGRAPH_1.NORMAL}</span>
+                {serviceContentItems}
               </div>
-              <div className="paragraph-wrapper">
-                <span className="highlight">{StringTable.ABOUT.MAIN.PARAGRAPH_2.HIGHLIGHT}</span>
-                <span className="normal">{StringTable.ABOUT.MAIN.PARAGRAPH_2.NORMAL}</span>
-              </div>
-              <div className="paragraph-wrapper">
-                <span className="highlight">{StringTable.ABOUT.MAIN.PARAGRAPH_3.HIGHLIGHT}</span>
-                <span className="normal">{StringTable.ABOUT.MAIN.PARAGRAPH_3.NORMAL}</span>
-              </div>
-            </div>
+
             </div>
           </div>
 
--- src/service/StringTable.js
+++ src/service/StringTable.js
@@ -20,6 +20,97 @@ export const StringTable = {
         , NORMAL: ' are passionate domestic/international students who are studying in various majors and actively participating in diverse activities to gain experience. Through the eyes of these students, ROUTE provides strategically helpful insights and solutions to our clients while enabling our members to acquire practical experiences. To conclude, ROUTE pursues a complementary relationship with our clients based on active communication and cooperation.'
       }
     }
+  },
+
+  SERVICE: {
+    TITLE: "SERVICE",
+    DESCRIPTION: "HOW DO WE OFFER STRATEGY",
+    MAIN: {
+      CARDS: [
+        {
+          TITLE: "MARKET RESEARCH",
+          CONTENTS: [
+            {
+              type: "paragraph",
+              items: [
+                `Our working process involves providing our clients a carefully coordinated market research.`
+                , `The market research will be based on SEPTEmber(Social / Economic / Political / Technological / Environmental) Analysis of the client-relevant industry and ROUTE will also provide market segmentation analysis to facilitate the stakeholder return process.`
+                , `In addition to the above, ROUTE will also provide competitor and customer needs analysis.`
+              ]
+            }
+          ]
+        },
+
+        {
+          TITLE: "DATA ANALYTICS",
+          CONTENTS: [
+            {
+              type: "paragraph",
+              items: [
+                `For the best business strategy recommendation, ROUTE will provide an in-depth analysis of our client’s internal structure and performance.`
+                , `Some of the analytics that the organization will provide, but are not limited to, the following: accounting, asset management, sales performance, forecasting.`
+              ]
+            }
+          ]
+        },
+        
+        {
+          TITLE: "STRATEGY RECOMMENDATION",
+          CONTENTS: [
+            {
+              type: "paragraph",
+              items: [
+                `Our goal is to provide customized consulting services. Therefore, we are willing to give a strategic recommendation for any challenges our client firms are facing. We focus on scoping projects that can address our clients' unique situations including those listed below. `
+              ]
+            },
+            {
+              type: "subtitle",
+              items: [
+                `Examples of Our Services`
+              ]
+            },
+            {
+              type: "block",
+              items: [
+                {
+                  title: 'Building Market Strategy',
+                  descriptions: [
+                    'Market Strategy Competitor analysis, funding models, and data collection'
+                  ]
+                },
+                {
+                  title: 'Marketing and Reaching Customers through Media',
+                  descriptions: [
+                    'Social Media impact measurement, website and user experience design, general marketing strategy, and customer analytics'
+                  ]
+                },
+                {
+                  title: 'Market Entry and Expansion',
+                  descriptions: [
+                    'Go-to-market strategy for new products and services, Expanding market for existing services'
+                  ]
+                },
+                {
+                  title: 'Operational Support',
+                  descriptions: [
+                    'Supply chain strategy, cost reduction, business model transformation, and process optimization'
+                  ]
+                },
+                {
+                  title: 'Financial Analysis',
+                  descriptions: [
+                    'Analyzing financial statements and examining performance'
+                  ]
+                }
+              ]
+            },
+
+          ]
+        },
+
+
+      ]
+    }
   }
 }
 
