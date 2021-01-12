1c39e02ea5e900c0a7614ac4bd6021fe3a0ef3e0
이재영(도마) <jaeleeps@jaranda.kr>
2021-01-12 22:05:41 +0900

    youtube ifram video inserted

--- src/pages/about/AboutPageComponent/AboutPageComponent.js
+++ src/pages/about/AboutPageComponent/AboutPageComponent.js
@@ -10,26 +10,34 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 class AboutPageComponent extends Component {
   render() {
     const paragraphs = StringTable.ABOUT.MAIN.PARAGRAPHS.map((para, index) => {
-      
-      const descriptionImage = index === 0
-        ? (
-          <div className="description-wrapper" style={{backgroundImage: `url(${ImageTable.ABOUT_MEANING_IMAGE.IMG_02})`}}></div>
-        )
-        : index === 1
-          ? <div className="description-wrapper" style={{backgroundImage: `url(${ImageTable.ABOUT_MEANING_IMAGE.IMG_01})`}}></div>
-          : null;
+      const descriptionImage =
+        index === 0 ? (
+          <div
+            className="description-wrapper"
+            style={{
+              backgroundImage: `url(${ImageTable.ABOUT_MEANING_IMAGE.IMG_02})`,
+            }}
+          ></div>
+        ) : index === 1 ? (
+          <div
+            className="description-wrapper"
+            style={{
+              backgroundImage: `url(${ImageTable.ABOUT_MEANING_IMAGE.IMG_01})`,
+            }}
+          ></div>
+        ) : null;
 
-      
       return (
         <div className="paragraph-wrapper">
           <span className="highlight" style={{ color: para.highlight_color }}>
             {para.HIGHLIGHT}
           </span>
           <span className="normal">{para.NORMAL}</span>
-          { descriptionImage }
+          {descriptionImage}
         </div>
-      )
+      );
     });
+
     return (
       <div className="about-page-component-wrapper page-component-wrapper">
         <div className="about-page-main-content-wrapper">
@@ -44,7 +52,22 @@ class AboutPageComponent extends Component {
                 <div className="title-line"></div>
               </div>
 
-              <div className="logo-wrapper"></div>
+              {/* <div className="logo-wrapper"></div> */}
+
+              {/* <div className="youtube_wrapper">{youtube_wrapper}</div> */}
+
+              <div className="youtube_wrapper">
+                <iframe
+                  width="100%"
+                  height="100%"
+                  src="https://www.youtube.com/embed/KPP42p9TQT8?list=PLErqheOv0D5uYsfeMOGb-hdWe8IiO8PYz"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+
+              {/* <iframe width="1280" height="720" src="https://www.youtube.com/embed/KPP42p9TQT8?list=PLErqheOv0D5uYsfeMOGb-hdWe8IiO8PYz" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
 
               {/* <div className="description-img-wrapper">
                 <Carousel showArrows={true}>
